40a7157f9a1e582317e26a70a10ead862e3c2063
Rafael Maia <rafael.augusto.fm@outlook.com>
2021-08-16 14:57:46 +0000

    Configuring Google Fonts: Poppins Regular 400 + Semi-bold 600.

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,15 +1,9 @@
-import styled from 'styled-components'
 import { GlobalStyle } from './styles/global'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257E6;
-`
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
       <GlobalStyle />
     </div>
   );
