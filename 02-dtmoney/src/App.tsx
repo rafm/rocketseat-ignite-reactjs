b3064f11b4849fa679668401ac170dbb73b1cef9
Rafael Maia <rafael.augusto.fm@outlook.com>
2021-08-16 19:22:35 +0000

    Extracting NewTransactionModal logic into a new component.

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
+import Modal from 'react-modal'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
-import Modal from 'react-modal'
 import { GlobalStyle } from './styles/global'
+import { NewTransactionModal } from './components/NewTransactionModal'
 
 Modal.setAppElement('#root')
 
@@ -23,12 +24,7 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
-        isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
 
       <GlobalStyle />
     </>
