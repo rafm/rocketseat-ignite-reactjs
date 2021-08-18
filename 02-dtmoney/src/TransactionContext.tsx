7b7c87874c66d8172676c77e743cc75a5290029a
Rafael Maia <rafael.augusto.fm@outlook.com>
2021-08-18 20:45:35 +0000

    Moving create transaction logic into TransactionContext to allow reusing it across the components.

--- 02-dtmoney/src/TransactionContext.tsx
+++ 02-dtmoney/src/TransactionContext.tsx
@@ -10,11 +10,20 @@ interface Transaction {
     createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
 interface TransactionContextProps {
     children: ReactNode;
 }
 
-export const TransactionContext = createContext<Transaction[]>([])
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+)
 
 export function TransactionProvider({ children }: TransactionContextProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -24,8 +33,12 @@ export function TransactionProvider({ children }: TransactionContextProps) {
             .then(response => setTransactions(response.data.transactions))
     }, [])
 
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction)
+    }
+
     return (
-        <TransactionContext.Provider value={transactions}>
+        <TransactionContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionContext.Provider>
     )
