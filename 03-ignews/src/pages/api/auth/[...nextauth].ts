5914624ef81c2d6938cb02c1a0a7f193f3e5b22e
Rafael Maia <rafael.maia@arctouch.com>
2022-02-02 16:35:46 +0000

    Adding callback logic on sign in to register logged user if it's not already registered on the DB

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
@@ -9,5 +11,37 @@ export default NextAuth({
       authorization: "https://github.com/login/oauth/authorize?scope=read:user"
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user: { email } }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  },
 })
