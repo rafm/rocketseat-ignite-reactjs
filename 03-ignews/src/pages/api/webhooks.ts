2d6e34c351da015bb2c43f7e2fd7160a6b088652
Rafael Maia <rafael.maia@arctouch.com>
2022-02-08 21:04:10 +0000

    Webhooks: adding logic to manage subscription based on listen Stripe webhooks

--- 03-ignews/src/pages/api/webhooks.ts
+++ 03-ignews/src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { Readable } from "stream"
 import Stripe from "stripe"
 import { stripe } from "../../services/stripe"
+import { saveSubscription } from "./_lib/manageSubscription"
 
 async function buffer(readable: Readable) {
     const chunks = []
@@ -22,7 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    "checkout.session.completed"
+    "checkout.session.completed",
+    "customer.subscription.updated",
+    "customer.subscription.deleted",
 ])
 
 export default async function webhooks(req: NextApiRequest, res: NextApiResponse) {
@@ -44,7 +47,35 @@ export default async function webhooks(req: NextApiRequest, res: NextApiResponse
     }
 
     if (relevantEvents.has(event.type)) {
-        console.log("Evento recebido", event.type)
+        try {
+            switch (event.type) {
+                case "customer.subscription.updated":
+                case "customer.subscription.deleted":
+                    const subscription = event.data.object as Stripe.Subscription
+
+                    await saveSubscription(
+                        subscription.id,
+                        subscription.customer.toString(),
+                        false
+                    )
+
+                    break
+                case "checkout.session.completed":
+                    const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                    await saveSubscription(
+                        checkoutSession.subscription.toString(),
+                        checkoutSession.customer.toString(),
+                        true
+                    )
+
+                    break
+                default:
+                    throw new Error("Unhandled event.")
+            }
+        } catch (err) {
+            return res.json({ error: "Webhook handler failed." })
+        }
     }
     
     return res.json({ received: true })
