8077ee713ee165ed740ba15f6372de130e36bcfa
Rafael Maia <rafael.maia@arctouch.com>
2022-06-07 23:40:29 +0000

    Creating component ActiveLink to encapsulate Header's active links logic

---
+++ 03-ignews/src/components/ActiveLink/index.tsx
@@ -0,0 +1,21 @@
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+import { cloneElement, ReactElement } from 'react'
+
+interface ActiveLinkProps extends LinkProps {
+    children: ReactElement;
+    activeClassName: string;
+}
+
+export default function ActiveLink({ children, activeClassName, ...props }: ActiveLinkProps) {
+    const { asPath } = useRouter()
+    const className = asPath === props.href ? activeClassName : ""
+
+    return (
+        <Link {...props}>
+            {cloneElement(children, {
+                className
+            })}
+        </Link>
+    )
+}
--- 03-ignews/src/components/Header/index.tsx
+++ 03-ignews/src/components/Header/index.tsx
@@ -1,22 +1,19 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import ActiveLink from '../ActiveLink'
 
 export function Header() {
-    const { asPath } = useRouter()
-
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <Link href="/">
-                        <a className={asPath === "/" ? styles.active : ""}>Home</a>
-                    </Link>
-                    <Link href="/posts">
-                        <a className={asPath === "/posts" ? styles.active : ""}>Posts</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
