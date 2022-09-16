9ca23e101776a8e45dd98b327384e2f11bfb9052
Rafael Maia <rafael.augusto.fm@outlook.com>
2022-09-16 02:22:08 +0000

    Header: making responsive design

--- 04-dashgo/src/components/Header/Profile.tsx
+++ 04-dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Rafael Maia</Text>
-                <Text color="gray.300" fontSize="small">
-                    rafael.augusto.fm@outlook.com
-                </Text>
-            </Box>
+            { showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Rafael Maia</Text>
+                    <Text color="gray.300" fontSize="small">
+                        rafael.augusto.fm@outlook.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Rafael Maia" src="https://github.com/rafm.png" />
         </Flex>
